b94f12763ddeefe67c716f0db8e664279695c6fa
rafaelnascimentosilva <rafaelnasc.silv@hotmail.com>
2018-12-10 10:50:57 -0300

    primeiro commit

---
+++ src/main/java/com/eventoapp/eventoapp/controller/EventoController.java
@@ -0,0 +1,61 @@
+package com.eventoapp.eventoapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.eventoapp.eventoapp.models.Convidado;
+import com.eventoapp.eventoapp.models.Evento;
+import com.eventoapp.eventoapp.repository.ConvidadoRepository;
+import com.eventoapp.eventoapp.repository.EventoRepository;
+
+@Controller
+public class EventoController {
+
+	@Autowired
+	private EventoRepository er;
+	@Autowired
+	private ConvidadoRepository cr;
+
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.GET)
+	public String form() {
+		return "evento/formEvento";
+	}
+
+	@RequestMapping(value = "/cadastrarEvento", method = RequestMethod.POST)
+	public String form(Evento evento) {
+		er.save(evento);
+		return "redirect:cadastrarEvento";
+	}
+
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("index");
+		Iterable<Evento> eventos = er.findAll();
+		mv.addObject("eventos", eventos);
+		return mv;
+	}
+
+	@RequestMapping(value = "/{codigo}", method = RequestMethod.GET)
+	public ModelAndView detalhesEventoGet(@PathVariable("codigo") long codigo) {
+		ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+		Evento evento = er.findByCodigo(codigo);
+		mv.addObject("evento", evento);
+		Iterable<Convidado> convidados = cr.findByEvento(evento);
+		mv.addObject("convidados", convidados);
+		return mv;
+	}
+
+	@RequestMapping(value = "/{codigo}", method = RequestMethod.POST)
+	public String detalhesEventoPost(@PathVariable("codigo") long codigo, Convidado convidado) {
+		Evento evento = er.findByCodigo(codigo);
+		convidado.setEvento(evento);
+		cr.save(convidado);
+	
+
+		return "redirect:/{codigo}";
+	}
+}
